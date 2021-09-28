a17ddaa7bcf1b248c1d7c6ffa70dc9d4625d539c
Gabriela Mattos <gabrielamattosde@gmail.com>
2021-09-28 14:42:12 -0300

    colocando o menu com responsividade e interação

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 
 }
 
@@ -9,6 +12,9 @@ let shoppingCart = document.querySelector('.shopping-cart');
 
 document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
+    searchForm.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 
 }
 
@@ -16,12 +22,27 @@ let loginForm = document.querySelector('.login-form');
 
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
 
 }
 
-let navBar = document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar');
 
-document.querySelector('#login-btn').onclick = () =>{
-    Navbar.classList.toggle('active');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.toggle('active');
+
+
+}
+
+window.onscroll = () =>{
+    shoppingCart.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 
 }
