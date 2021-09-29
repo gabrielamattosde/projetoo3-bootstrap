bc69eb58765435d57808e1f65f1e9c6887dde7dc
Gabriela Mattos <gabrielamattosde@gmail.com>
2021-09-29 11:03:28 -0300

    Finalização da edição dos produtos com JS

--- script.js
+++ script.js
@@ -35,8 +35,6 @@ document.querySelector('#menu-btn').onclick = () =>{
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
-
-
 }
 
 window.onscroll = () =>{
@@ -44,5 +42,26 @@ window.onscroll = () =>{
     searchForm.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
-
 }
+
+
+var swiper = new Swiper(".produtos-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
